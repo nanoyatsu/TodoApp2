66e50b451d20c2c55a20a872c1d7b902a1a5394f
nanoyatsu <34255833+nanoyatsu@users.noreply.github.com>
2022-09-21 00:35:38 +0900

    依存更新とか

--- app/src/main/java/com/nanoyatsu/todoapp2/ui/taskList/TaskList.kt
+++ app/src/main/java/com/nanoyatsu/todoapp2/ui/taskList/TaskList.kt
@@ -1,9 +1,7 @@
 package com.nanoyatsu.todoapp2.ui.taskList
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
@@ -11,8 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nanoyatsu.todoapp2.data.db.entity.Task
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.onEach
 
 @Composable
 fun TaskListRoute() {
@@ -24,19 +20,19 @@ fun TaskListRoute() {
 @Composable
 fun TaskList() {
     val vm: TaskListViewModel = viewModel()
-    val taskList = listOf<Task>(
-        Task(1, "task-no-namae1", true),
-        Task(2, "task-no-namae2", false),
-        Task(3, "task-no-namae3", true),
-        Task(4, "task-no-namae4", false),
-        Task(5, "task-no-namae5", true),
-        Task(6, "task-no-namae6", false),
-        Task(7, "task-no-namae7", true),
-        Task(8, "task-no-namae8", false),
-        Task(9, "task-no-namae9", true),
-        Task(10, "task-no-namae10", false),
+    val taskList = listOf(
+        Task(1, "task-no-name1", true),
+        Task(2, "task-no-name2", false),
+        Task(3, "task-no-name3", true),
+        Task(4, "task-no-name4", false),
+        Task(5, "task-no-name5", true),
+        Task(6, "task-no-name6", false),
+        Task(7, "task-no-name7", true),
+        Task(8, "task-no-name8", false),
+        Task(9, "task-no-name9", true),
+        Task(10, "task-no-name10", false),
     )
-    LazyColumn() {
+    LazyColumn {
         //vm.allTask.distinctUntilChanged().onEach {
         items(taskList) { task -> TaskItem(task = task) }
         //}
@@ -45,7 +41,7 @@ fun TaskList() {
 
 @Composable
 fun TaskItem(task: Task) {
-    Row() {
+    Row {
         Text(text = task.label)
         Checkbox(checked = task.completed, onCheckedChange = {})
     }
@@ -59,5 +55,5 @@ fun TaskItem(task: Task) {
 @Preview
 @Composable
 fun TaskItemPreview() {
-    TaskItem(task = Task(1, "task-lavel", true))
+    TaskItem(task = Task(1, "task-label", true))
 }
